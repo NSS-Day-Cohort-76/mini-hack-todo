bacd9f1de2effa3665e0ae88c9fcb034432bf55a
Brit Beaty <britneybeaty@gmail.com>
2025-05-23 13:52:49 -0500

    added Navbar component

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,8 +1,38 @@
+import { Link, Navigate } from "react-router-dom";
+
 export const Navbar = () => {
 
-    return (<>
-    
-    </>)
+    return (<ul className="nav-bar">
+            <li className="nav-bar-item-logo">
+                    <h3>to-doodle</h3>
+            </li>
+            <li className="nav-bar-item">
+                <Link className="nav-bar-link" to="/">
+                   Home
+                </Link>
+            </li>
+            <li className="nav-bar-item">
+                <Link className="nav-bar-link" to="/Leaderboard">
+                   Leaderboard
+                </Link>
+            </li>
+            {localStorage.getItem("task_user") ? (
+                <li className="nav-bar-item">
+                    <Link
+                        className="nav-bar-link"
+                        to=""
+                        onClick={() => {
+                            localStorage.removeItem("task_user");
+                            Navigate("/login", { replace: true });
+                        }}
+                    >
+                        Logout
+                    </Link>
+                </li>
+            ) : (
+                ""
+            )}
+        </ul>)
 
 
 
